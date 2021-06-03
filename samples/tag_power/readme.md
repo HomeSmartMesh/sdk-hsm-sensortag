dc53692139f5b909e8a0969909fe6a1b5dc698ff
wass <wassim.filali@gmail.com>
2021-06-03 12:13:53 +0200

    low power fix at 3 uA

--- samples/tag_power/readme.md
+++ samples/tag_power/readme.md
@@ -1,7 +1,11 @@
 ## usage
+low power tested with `prj-simple.conf` k_sleep @3uA
+
 ```bash
 west build -b nrf52840_sensortag -t guiconfig
+west build -b nrf52840_sensortag
 west build -b nrf52840_sensortag -- -DCONF_FILE=prj.conf
+
 west build -b nrf52840_sensortag -- -DCONF_FILE=prj-low.conf
 west flash
 ```
@@ -11,9 +15,79 @@ device_set_power_state has no influence on GPIO as PGIO has no enable and no inf
 ## Documentation
 https://www.homesmartmesh.com/docs/microcontrollers/nrf52/thread_sensortag/#tag_power
 
+# Nordic ticket
+https://devzone.nordicsemi.com/f/nordic-q-a/75774/nrf52840-zephyr-tickless-low-power-idle-always-ticking
+
+## call tree
+k_sleep (kernel.h) -> z_impl_k_sleep(sched.c) -> 
+K_FOREVER : -> k_thread_suspend -> z_impl_k_thread_suspend -> z_reschedule_unlocked -> z_reschedule_irqlock
+100 ms    : -> z_tick_sleep
+
 # Lowe power perf
 * normal sleep : ~ 3.5 mA
 * sleep with releasing HF clock : ~ 1.8 mA
+
+## low power sleep attempt
+
+```c
+//the fix is just not to use this function
+void lp_sleep_ms(int sleep_ms)
+{
+	sys_clock_set_timeout(k_ms_to_ticks_ceil32(sleep_ms),false);//sleep @2.9 uA
+	//----------sleep prepare--------------
+	nrf_clock_task_trigger(NRF_CLOCK,NRF_CLOCK_TASK_HFCLKSTOP);
+	nrf_power_task_trigger(NRF_POWER,NRF_POWER_TASK_LOWPWR);
+	__WFE();
+	__SEV();// Clear the internal event register.
+	__WFE();
+	//----------sleep wakeup--------------
+    nrf_clock_event_clear(NRF_CLOCK,NRF_CLOCK_EVENT_HFCLKSTARTED);
+    nrf_clock_task_trigger(NRF_CLOCK,NRF_CLOCK_TASK_HFCLKSTART);
+	while(!nrf_clock_hf_is_running(NRF_CLOCK,NRF_CLOCK_HFCLK_HIGH_ACCURACY));
+}
+
+#if STOP_TEST_OFF
+	pm_power_state_force((struct pm_state_info){PM_STATE_SOFT_OFF, 0, 0});//PM_STATE_SOFT_OFF
+#endif
+
+```
+
+## main policy
+```c
+#ifdef CONFIG_PM
+static const struct pm_state_info pm_min_residency[] ={
+	{PM_STATE_RUNTIME_IDLE		,1 ,1000 , 0},		//  1 ms
+	{PM_STATE_STANDBY			,2 ,20000 , 0},	// 20 ms
+	{PM_STATE_SUSPEND_TO_RAM	,3 ,100000 , 0},	// 100 ms
+	{PM_STATE_SUSPEND_TO_DISK	,4 ,200000 , 0}	// 200 ms
+};
+
+struct pm_state_info pm_policy_next_state(int32_t ticks)
+{
+	int i;
+	static struct pm_state_info current = {PM_STATE_ACTIVE,0, 1000};
+
+	for (i = ARRAY_SIZE(pm_min_residency) - 1; i >= 0; i--) {
+		if (!pm_constraint_get(pm_min_residency[i].state)) {
+			continue;
+		}
+
+		if ((ticks == K_TICKS_FOREVER) || (ticks >= k_us_to_ticks_ceil32(pm_min_residency[i].min_residency_us))) 
+		{
+			if(current.state != pm_min_residency[i].state)
+			{
+				LOG_INF("Selected power state %d (ticks: %d, min_residency: %u)",pm_min_residency[i].state, ticks,pm_min_residency[i].min_residency_us);
+				current = pm_min_residency[i];
+			}
+			return pm_min_residency[i];
+		}
+	}
+
+	//LOG_DBG("No suitable power state found!");
+	return (struct pm_state_info){PM_STATE_ACTIVE, 0, 0};
+}
+#endif
+```
 ## Zephyr patch
 content of `zephyr\soc\arm\nordic_nrf\nrf52\power.c`
 
--- samples/tag_power/src/main.c
+++ samples/tag_power/src/main.c
@@ -26,29 +26,6 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 #define debug_down()	gpio_pin_set(gpio_dev, DEBUG_PIN_APP, 0)
 #define debug_os_down()	gpio_pin_set(gpio_dev, DEBUG_PIN_OS, 0)
 
-void app_sleep_prepare()
-{
-	nrf_clock_task_trigger(NRF_CLOCK,NRF_CLOCK_TASK_HFCLKSTOP);
-
-	nrf_power_task_trigger(NRF_POWER,NRF_POWER_TASK_LOWPWR);
-
-	DEBUG_PIN_29_SET;
-
-	__WFE();
-	// Clear the internal event register.
-	__SEV();
-	__WFE();
-}
-
-void app_sleep_wakeup()
-{
-    nrf_clock_event_clear(NRF_CLOCK,NRF_CLOCK_EVENT_HFCLKSTARTED);
-    nrf_clock_task_trigger(NRF_CLOCK,NRF_CLOCK_TASK_HFCLKSTART);
-	while(!nrf_clock_hf_is_running(NRF_CLOCK,NRF_CLOCK_HFCLK_HIGH_ACCURACY));
-	DEBUG_PIN_29_CLEAR;
-}
-
-
 const struct device *gpio_dev;
 void gpio_pin_init()
 {
@@ -63,48 +40,6 @@ void gpio_pin_init()
 	}
 }
 
-void test_sleep(uint32_t sleep_ms)
-{
-	LOG_INF("going to sleep %u ms",sleep_ms);
-	debug_up();
-	k_sleep(K_MSEC(sleep_ms));
-	debug_down();
-}
-
-#ifdef CONFIG_PM
-static const struct pm_state_info pm_min_residency[] ={
-	{PM_STATE_RUNTIME_IDLE		,1 ,1000 , 0},		//  1 ms
-	{PM_STATE_STANDBY			,2 ,20000 , 0},	// 20 ms
-	{PM_STATE_SUSPEND_TO_RAM	,3 ,100000 , 0},	// 100 ms
-	{PM_STATE_SUSPEND_TO_DISK	,4 ,200000 , 0}	// 200 ms
-};
-
-struct pm_state_info pm_policy_next_state(int32_t ticks)
-{
-	int i;
-	static struct pm_state_info current = {PM_STATE_ACTIVE,0, 1000};
-
-	for (i = ARRAY_SIZE(pm_min_residency) - 1; i >= 0; i--) {
-		if (!pm_constraint_get(pm_min_residency[i].state)) {
-			continue;
-		}
-
-		if ((ticks == K_TICKS_FOREVER) || (ticks >= k_us_to_ticks_ceil32(pm_min_residency[i].min_residency_us))) 
-		{
-			if(current.state != pm_min_residency[i].state)
-			{
-				LOG_INF("Selected power state %d (ticks: %d, min_residency: %u)",pm_min_residency[i].state, ticks,pm_min_residency[i].min_residency_us);
-				current = pm_min_residency[i];
-			}
-			return pm_min_residency[i];
-		}
-	}
-
-	//LOG_DBG("No suitable power state found!");
-	return (struct pm_state_info){PM_STATE_ACTIVE, 0, 0};
-}
-#endif
-
 void main(void)
 {
 	gpio_pin_init();
@@ -112,29 +47,20 @@ void main(void)
 	debug_os_down();
 	LOG_INF("Hello Power management");
 
-	test_sleep(1);
-	test_sleep(1);
+	debug_up();
+	k_sleep(K_MSEC(1));
+	debug_down();
+	k_sleep(K_MSEC(1));
+	debug_up();
 
-	//in this next 250 ms sleep
-	//Question : Why despite the `CONFIG_TICKLESS_KERNEL=y` the os idle task keeps ticking through RTC @ 297 us period
-	//https://devzone.nordicsemi.com/f/nordic-q-a/75774/nrf52840-zephyr-tickless-low-power-idle-always-ticking
-	//therefore using 'k_sleep' it's not possible to reach a long average low power mode
-	test_sleep(250);
-
-	#if STOP_TEST_OFF
-		pm_power_state_force((struct pm_state_info){PM_STATE_SOFT_OFF, 0, 0});//PM_STATE_SOFT_OFF
-	#endif
+	k_sleep(K_MSEC(250));
+	debug_down();
 
 	while (1) {
-		//first cycle @268 us, then second @5 sec (washing out the last os tick left)
 		debug_up();
-		sys_clock_set_timeout(k_ms_to_ticks_ceil32(5000),false);//5 sec @2.9 uA
-		app_sleep_prepare();
-		app_sleep_wakeup();
+		k_sleep(K_MSEC(5000));//@3 uA
 		debug_down();
-		#if LOOP_WITH_SLEEP
-			test_sleep(100);//if this line is enabled it prevents the os from stoping the idle task ticks
-		#endif
-		k_busy_wait(1000*1000);//1 sec @3 mA
+		k_busy_wait(2000*1000);//@3 mA
 	}
 }
+
