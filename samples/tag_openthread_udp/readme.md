ec3d0518d4963f368c3dcbf1f4804e4b18fd1f42
wass <wassim.filali@gmail.com>
2021-06-05 13:30:27 +0200

    etst with radio off

--- samples/tag_openthread_udp/readme.md
+++ samples/tag_openthread_udp/readme.md
@@ -1,9 +1,24 @@
 ## usage
 ```bash
-west build -t guiconfig
-west build -b nrf52840_sensortag -- -DCONF_FILE="prj.conf overlay-shell.conf"
+west build -b nrf52840_sensortag -t guiconfig
+west build -b nrf52840_sensortag
+west build -b nrf52840_sensortag -- -DCONF_FILE=prj.conf
+west build -b nrf52840_sensortag -- -DCONF_FILE="prj.conf overlay-tracing.conf"
+west build -b nrf52840_sensortag -- -DCONF_FILE="prj.conf overlay-debug.conf overlay-tracing.conf"
 west flash
 ```
 
 ## Documentation
 https://www.homesmartmesh.com/docs/microcontrollers/nrf52/thread_sensortag/#tag_openthread_udp
+
+
+
+otSysDeinit -> nrf5RadioDeinit -> nrf_802154_deinit -> nrf_802154_core_deinit -> nrf_802154_trx_disable -> nrf_radio_power_set -> 
+
+
+* otSysDeinit : only in platoform includes
+* nrf5RadioDeinit : not compiled for nRF52840
+* nrf_802154_deinit : calls nrf_802154_core_deinit
+* nrf_802154_core_deinit : conditional call for nrf_802154_trx_disable();
+* nrf_802154_init : does not call nrf_802154_trx_enable
+* nrf_802154_trx_disable : on nrf_802154_trx.h which is not exposed
--- samples/tag_openthread_udp/src/main.c
+++ samples/tag_openthread_udp/src/main.c
@@ -2,25 +2,90 @@
 #include <zephyr.h>
 #include <logging/log.h>
 #include <net/socket.h>
+#include <net/net_if.h>
 #include <stdio.h>
+#include <drivers/gpio.h>
+
+#include <net/openthread.h>
+#include <openthread/thread.h>
+
+#include "hal/nrf_radio.h"
 
 #include "udp_client.h"
 
-LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
-
+LOG_MODULE_REGISTER(main, LOG_LEVEL_NONE);
 #define SLEEP_TIME_MS   10000
+
+#define DEBUG_PIN_APP 	 2
+#define DEBUG_PIN_OS	29
+
+#define APP_SET 	gpio_pin_set(gpio_dev, DEBUG_PIN_APP, 1)	
+#define APP_CLEAR 	gpio_pin_set(gpio_dev, DEBUG_PIN_APP, 0)
+#define LOOP_SET 	gpio_pin_set(gpio_dev, DEBUG_PIN_OS, 1)
+#define LOOP_CLEAR 	gpio_pin_set(gpio_dev, DEBUG_PIN_OS, 0)
+
+const struct device *gpio_dev;
+void gpio_pin_init()
+{
+	gpio_dev = device_get_binding(DT_LABEL(DT_NODELABEL(gpio0)));
+	int ret = gpio_pin_configure(gpio_dev, DEBUG_PIN_APP, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		LOG_ERR("gpio_pin_configure() failed");
+	}
+	ret = gpio_pin_configure(gpio_dev, DEBUG_PIN_OS, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		LOG_ERR("gpio_pin_configure() failed");
+	}
+}
 
 void main(void)
 {
-	LOG_INF("Hello from Sensor Tag");
+	gpio_pin_init();
+	APP_CLEAR;
+	LOOP_CLEAR;
+	k_sleep(K_MSEC(10));
 
+	APP_SET;
+	k_sleep(K_MSEC(10));
+	APP_CLEAR;
+
+	LOG_INF("Hello openthread udp");
+	k_sleep(K_MSEC(10));
+
+	struct otInstance *openthread = openthread_get_default_instance();
+	struct net_if * net = net_if_get_default();
+
+	long unsigned int id0 = NRF_FICR->DEVICEID[0];//just for type casting and readable printing
+	long unsigned int id1 = NRF_FICR->DEVICEID[1];
 	int count = 0;
 	while (1) {
-		LOG_INF("sleeping 10 sec");
-		k_msleep(SLEEP_TIME_MS);
-		char message[20];
-		int size = sprintf(message,"Tag loop (%d)\n",count);
+		LOOP_SET;
+		LOG_INF("starting loop (%d)",count);
+		
+		char message[250];
+		int size = sprintf(message,"thread_tags/%04lX%04lX{\"alive\":%d}",id0,id1,count);
+
+		APP_SET;
+		if(!net_if_is_up(net))
+		{
+			nrf_radio_power_set(NRF_RADIO,true);
+			net_if_up(net);
+			otThreadSetEnabled(openthread,true);
+		}
+		APP_CLEAR;
 		send_udp(message, size);
-		LOG_INF("end of loop: %d",count++);
+		k_sleep(K_MSEC(100));
+		APP_SET;		//loop pulse 2 send_udp
+		otThreadSetEnabled(openthread,false);
+		net_if_down(net);
+		nrf_radio_power_set(NRF_RADIO,false);
+		APP_CLEAR;
+
+		printf("%s\n",message);
+		LOG_INF("sleeping 1 sec");
+		count++;
+		LOOP_CLEAR;
+		k_sleep(K_MSEC(3000));
+
 	}
 }
