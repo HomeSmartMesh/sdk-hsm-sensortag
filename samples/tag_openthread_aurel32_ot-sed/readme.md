930a8db25bd3a35b79ca8a1573b78814a2719082
wass <wassim.filali@gmail.com>
2021-06-06 11:46:05 +0200

    debug sed with gpio

--- samples/tag_openthread_aurel32_ot-sed/readme.md
+++ samples/tag_openthread_aurel32_ot-sed/readme.md
@@ -1,1 +1,3 @@
+west build -b nrf52840_sensortag -- -DCONF_FILE=prj.conf
 west build -b nrf52840_sensortag -- -DCONF_FILE="prj.conf overlay-network.conf"
+west build -b nrf52840_sensortag -- -DCONF_FILE="prj.conf overlay-network.conf overlay-piodebug.conf"
--- samples/tag_openthread_aurel32_ot-sed/src/main.c
+++ samples/tag_openthread_aurel32_ot-sed/src/main.c
@@ -15,6 +15,31 @@
 #define SERVER_ADDR		"ff02::1"
 #define SERVER_PORT		4242
 
+#ifdef CONFIG_GPIO
+	#include <drivers/gpio.h>
+	#define DEBUG_PIN_APP 	 2
+	#define DEBUG_PIN_LOOP	29
+
+	#define APP_SET 	gpio_pin_set(gpio_dev, DEBUG_PIN_APP, 1)
+	#define APP_CLEAR 	gpio_pin_set(gpio_dev, DEBUG_PIN_APP, 0)
+	#define LOOP_SET 	gpio_pin_set(gpio_dev, DEBUG_PIN_LOOP, 1)
+	#define LOOP_CLEAR 	gpio_pin_set(gpio_dev, DEBUG_PIN_LOOP, 0)
+
+	const struct device *gpio_dev;
+	void gpio_pin_init()
+	{
+		gpio_dev = device_get_binding(DT_LABEL(DT_NODELABEL(gpio0)));
+		gpio_pin_configure(gpio_dev, DEBUG_PIN_APP, GPIO_OUTPUT_ACTIVE);
+		gpio_pin_configure(gpio_dev, DEBUG_PIN_LOOP, GPIO_OUTPUT_ACTIVE);
+	}
+#else
+	#define APP_SET
+	#define APP_CLEAR
+	#define LOOP_SET
+	#define LOOP_CLEAR
+	#define gpio_pin_init()
+#endif
+
 const struct device *temp_sensor = DEVICE_DT_GET(DT_INST(0, nordic_nrf_temp));
 
 void main(void)
@@ -24,43 +49,58 @@ void main(void)
 	int sock;
 	int ret;
 
-        otInstance *otdev = openthread_get_default_instance();
-        otPlatRadioSetTransmitPower(otdev, 8);
+	gpio_pin_init();
+	APP_CLEAR;
+	LOOP_CLEAR;
 
 	k_sleep(K_SECONDS(2));
 
+	int count_main = 0;
 	while (1) {
+		//init pulse
+		APP_SET;
+		count_main++;
 		addr6.sin6_family = AF_INET6;
 		addr6.sin6_port = htons(SERVER_PORT);
 		inet_pton(AF_INET6, SERVER_ADDR, &addr6.sin6_addr);
 		addr = (struct sockaddr *)&addr6;
+		APP_CLEAR;
 
 		sock = socket(addr->sa_family, SOCK_DGRAM, IPPROTO_UDP);
 		if (sock < 0) {
 			continue;
 		}
+		//connect pulse
+		APP_SET;
 		ret = connect(sock, addr, sizeof(addr6));
 		if (ret < 0) {
 			continue;
 		}
+		APP_CLEAR;
+		int count_sub = 0;
 		while (1) {
-			char payload[30];
+			LOOP_SET;
+			count_sub++;
+			char payload[64];
 			struct sensor_value val;
 			sensor_sample_fetch(temp_sensor);
 			sensor_channel_get(temp_sensor, SENSOR_CHAN_DIE_TEMP, &val);
 			int i = val.val1;
 			int f = val.val2 / 10000;
-
-			snprintk(payload, sizeof(payload), "%d.%02d\n", i, f);
-
+			snprintk(payload, sizeof(payload), "{\"main\":%d,\"sub\":%d,\"die_temp\":%d.%02d}",
+												count_main,count_sub, i, f);
+			//send pulse
+			APP_SET;
 			ret = send(sock, payload, strlen(payload), 0);
 			if (ret < 0) {
 				break;
 			}
-			k_sleep(K_SECONDS(15));
+			APP_CLEAR;
+			LOOP_CLEAR;
+			k_sleep(K_SECONDS(3));
 		}
 		
 		close(sock);
-		k_sleep(K_SECONDS(2));
+		k_sleep(K_SECONDS(1));
 	}
 }
